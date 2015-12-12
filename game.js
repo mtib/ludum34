667e9994c72f5ee88e7e0df06a59148e9589ecaf
Markus Becker <mtb@tibyte.net>
2015-12-12 04:55:54 +0100

    wip buttons

--- game.js
+++ game.js
@@ -1,25 +1,45 @@
+// Canvas Size
 var WIDTH = 1280;
 var HEIGHT = 720;
-var renderer = PIXI.autoDetectRenderer(WIDTH, HEIGHT, {antialiasing: false, transparent: false, resolution: 1});
-var stage = new PIXI.Container();
-renderer.backgroundColor = 0xFF00FF;
-document.getElementById("game").appendChild(renderer.view);
-var fontConfig = {font: "30px 'rockfire'", fill: "#000000", align: "left"};
 
+// Initialize Renderer
+var renderer = PIXI.autoDetectRenderer(WIDTH, HEIGHT, {antialiasing: false, transparent: false, resolution: 1});
+renderer.backgroundColor = 0xFF00FF;
+
+// Master Containter
+var stage = new PIXI.Container();
+
+// Config goes here:
+var fontConfig = {font: "30px 'rockfire'", fill: "#000000", align: "left"};
+var relcenter = {x: 0.5, y: 0.5};
+
+// Layers
 var cBack = new PIXI.Container();
 var cMiddle = new PIXI.Container();
 var cFront = new PIXI.Container();
 var cGui = new PIXI.Container();
+/*
+    stage
+      |
+      +-- cBack
+      +-- cMiddle
+      +-- cFront
+      +-- cGui
 
+Drawn from top to bottom
+*/
 stage.addChild(cBack);
 stage.addChild(cMiddle);
 stage.addChild(cFront);
 stage.addChild(cGui);
 
+// Version Text (top left)
 var versionText = new PIXI.Text("Version 0.01d", fontConfig);
 cGui.addChild(versionText);
 versionText.position = {x:10,y:10};
 
+// Keyboard IO
+// TODO on screen keys (interactive=true)
 var loadKey = keyboard(65);
 loadKey.press=function(){
     ingameLoad();
@@ -33,8 +53,12 @@ fuckKey.press=function(){
     versionText.text="FUCK YOU!";
 }
 
+
+// init Gamestate
 var gameState = new State();
 
+// Load Images
+// eg. assets/images/buttons/[load_btn.png, unload_btn.png]
 /*
 
 PIXI.loader
@@ -42,17 +66,19 @@ PIXI.loader
 
 */
 
-
+// Called just before rendering the first frame
 function setup(){
     renderStage();
 }
 
+// Class for Gamestate
 function State(){
     this.level = 0;
     this.name = "";
     this.mistakes = 0;
 }
 
+// Class for Ships
 function Ship(st, en, typ){
     this.start = st;
     this.end = en;
@@ -63,20 +89,25 @@ function Ship(st, en, typ){
     };
 }
 
+// Request Animation Frame
 function renderStage(){
     requestAnimationFrame(renderStage);
     renderLoop();
     renderer.render(stage);
 }
 
+// Called on "Load"-Press
 function ingameLoad(){
     versionText.text="loading";
 }
 
+// Called on "Unload"-Press
 function ingameUnload(){
     versionText.text="unloading";
 }
 
+// Called in-between rendering
+// should be used for logic
 function renderLoop(){
 
 }
