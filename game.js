e7957f86784d3987d789a50ea0ab32f5c44caf00
Markus Becker <mtb@tibyte.net>
2015-12-12 03:51:42 +0100

    engine

---
+++ game.js
@@ -0,0 +1,58 @@
+console.log("init")
+
+var WIDTH = 1280;
+var HEIGHT = 720;
+var renderer = PIXI.autoDetectRenderer(WIDTH, HEIGHT, {antialiasing: false, transparent: false, resolution: 1});
+var stage = new PIXI.Container();
+renderer.backgroundColor = 0xFFF12F;
+document.getElementById("game").appendChild(renderer.view);
+var fontConfig = {font: "20px "}
+
+var cBack = new PIXI.Container();
+var cMiddle = new PIXI.Container();
+var cFront = new PIXI.Container();
+var cGui = new PIXI.Container();
+
+stage.addChild(cBack);
+stage.addChild(cMiddle);
+stage.addChild(cFront);
+stage.addChild(cGui);
+
+var versionText = new PIXI.Text("Version 0.01d", {})
+
+var loadKey = keyboard(65);
+var unloadKey = keyboard(68);
+
+var gameState = new State();
+
+/*
+
+PIXI.loader
+    .add("..");
+
+*/
+
+
+function setup(){
+    renderStage();
+}
+
+function State(){
+    this.level = 0;
+    this.name = "";
+    this.mistakes = 0;
+}
+
+function renderStage(){
+    requestAnimationFrame(renderStage);
+    renderLoop();
+    renderer.render(stage);
+    console.log("render loop");
+}
+
+function renderLoop(){
+
+}
+
+// Start Rendering
+setup();
