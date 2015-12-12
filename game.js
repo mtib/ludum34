f53b618af7af3b9fb140ea1099f66bf5fbd42046
LFalch <lucas@wasd.dk>
2015-12-12 22:09:03 +0100

    Added comment

--- game.js
+++ game.js
@@ -250,7 +250,7 @@ function Ship(){
     this.die = function(){
         window.clearInterval(this.movement);
         cMiddle.removeChild(this.sprite);
-        testship = new Ship();
+        testship = new Ship(); // This seems very odd to hardcode
         var sum = this.leftlevel + this.middlelevel + this.rightlevel;
         gameState.mistakes += abs(this.end-sum);
         gameState.points += abs(this.start-sum);
