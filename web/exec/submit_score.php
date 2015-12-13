797fce24f0be2a5b7465680e33a633aee208c107
LFalch <lucas@wasd.dk>
2015-12-13 20:12:23 +0100

    Submit score PHP file

---
+++ web/exec/submit_score.php
@@ -0,0 +1,26 @@
+<?
+    require("connection.php");
+
+    $pts      = $_POST['points'];
+    $mistakes = $_POST['mistakes'];
+    $tm       = $_POST['time_taken'];
+
+    if(!(isset($pts) && isset($mistakes) && isset($tm))){
+        die('Invalid post request');
+    }
+
+    $con = getConnection();
+
+    $prep = $con->prepare("INSERT INTO `shipload`.`scores` (
+    `id` ,
+    `points` ,
+    `mistakes` ,
+    `time_taken`
+    )
+    VALUES (
+    NULL, ?, ?, ?
+    );");
+
+    $prep->bind_param('iii', $pts, $mistakes, $tm);
+    $prep->execute();
+?>
